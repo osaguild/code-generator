1106046f4335ed405eb0dc4b15f670ba3992a715
osaguild <nobuhisa.abe.0922@gmail.com>
2023-02-18 10:50:37 +0900

    add default and set max token

--- completion/index.ts
+++ completion/index.ts
@@ -6,22 +6,29 @@ export const generateCompletion = (completionName: string, message: string): Cre
     throw new Error(`Completion ${name} is not supported`);
   }
 
-  let prompt: string;
-  if (completion.name === "animal") {
-    const capitalizedAnimal = message[0].toUpperCase() + message.slice(1).toLowerCase();
-    prompt = completion.prompt.replace("{message}", capitalizedAnimal);
-  } else {
-    prompt = completion.prompt.replace("{message}", message);
-  }
+  const prompt =
+    completion.name === "default"
+      ? message
+      : completion.name === "animal"
+      ? completion.prompt.replace("{message}", message[0].toUpperCase() + message.slice(1).toLowerCase())
+      : completion.prompt.replace("{message}", message);
 
   return {
     model: completion.model,
     prompt,
     temperature: completion.temperature,
+    max_tokens: 1000,
   };
 };
 
 export const supportedCompletions: Completion[] = [
+  {
+    name: "default",
+    description: "no template",
+    model: "text-davinci-003",
+    temperature: 0,
+    prompt: "",
+  },
   {
     name: "animal",
     description: "Suggest three names for an animal that is a superhero",
@@ -29,12 +36,5 @@ export const supportedCompletions: Completion[] = [
     temperature: 0.6,
     prompt:
       "Suggest three names for an animal that is a superhero.\n\nAnimal: Cat\nNames: Captain Sharpclaw, Agent Fluffball, The Incredible Feline\nAnimal: Dog\nNames: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot\nAnimal: {message}\nNames:",
-  },
-  {
-    name: "hoge",
-    description: "hoge",
-    model: "text-davinci-003",
-    temperature: 1,
-    prompt: "hoge {message} hoge",
   },
 ];
